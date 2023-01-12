fa0633531111380b73e72d8b89599b9e3ca927a3
xAdrianCx <adrian.cotuna26@gmail.com>
2023-01-12 23:11:25 +0200

    Sequences

--- Sequences/moreprint.py
+++ Sequences/moreprint.py
@@ -2,4 +2,5 @@ name = "Tim"
 age = 10
 
 print(name, age, "Python", 2020)
-print(name, age, "Python", 2020, sep=", ")
+print((name, age, "Python", 2020))
+
---
+++ Sequences/tuples_intro.py
@@ -0,0 +1,14 @@
+albums = [("Welcome to my Nightmare", "Alice Cooper", 1975),
+          ("Bad Company", "Bad Company", 1974),
+          ("Nightflight", "Budgie", 1981),
+          ("More Mayhem", "Emilda May", 2011),
+          ("Ride the Lightning", "Metallica", 1984),
+          ]
+
+print(len(albums))
+
+for name, artist, year in albums:
+    print("Album: {}, Artist: {}, Year: {}"
+          .format(name, artist, year))
+
+
