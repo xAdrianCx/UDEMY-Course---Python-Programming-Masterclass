340588ad5bfbc87e4d2fc503fd8e026f744e4db1
xAdrianCx <adrian.cotuna26@gmail.com>
2023-01-13 16:27:58 +0200

    Sequences

--- Sequences/nested_data.py
+++ Sequences/nested_data.py
@@ -37,21 +37,3 @@ albums = [
      ]
      ),
 ]
-
-for name, artist, year, songs, in albums:
-    print("Album: {}, Artist: {}, Year: {}, Songs: {}"
-          .format(name, artist, year, songs))
-
-print()
-
-album = albums[3]
-print(album)
-
-songs = album[3]
-print(songs)
-
-song = songs[2]
-print(song)
-print(song[1])
-mayhem = albums[3][3][2][1]
-print(mayhem)
